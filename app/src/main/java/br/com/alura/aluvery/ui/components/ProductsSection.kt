f96deaa20feba138df273055399aab26c233d826
clauRTmfg <crta4.0@gmail.com>
2023-08-15 11:22:33 -0300

    Implementação de slots no código das seções

--- app/src/main/java/br/com/alura/aluvery/ui/components/ProductsSection.kt
+++ app/src/main/java/br/com/alura/aluvery/ui/components/ProductsSection.kt
@@ -23,30 +23,32 @@ fun ProductsSection(
     products: List<Product>,
     modifier: Modifier = Modifier,
 ) {
-    Column(modifier) {
-        Text(
-            text = title,
-            Modifier.padding(
-                start = 16.dp,
-                end = 16.dp
-            ),
-            fontSize = 20.sp,
-            fontWeight = FontWeight(400)
-        )
-        LazyRow(
-            Modifier
-                .padding(
-                    top = 8.dp
-                )
-                .fillMaxWidth(),
-            horizontalArrangement = Arrangement.spacedBy(16.dp),
-            contentPadding = PaddingValues(horizontal = 16.dp)
-        ) {
-            items(products) {p ->
-                ProductItem(product = p)
+    Section(
+        title = {
+            Text(
+                text = title,
+                Modifier.padding(
+                    start = 16.dp,
+                    end = 16.dp
+                ),
+                fontSize = 20.sp,
+                fontWeight = FontWeight(400)
+            )
+        },
+        content = {
+            LazyRow(
+                Modifier
+                    .fillMaxWidth(),
+                horizontalArrangement = Arrangement.spacedBy(16.dp),
+                contentPadding = PaddingValues(horizontal = 16.dp)
+            ) {
+                items(products) { p ->
+                    ProductItem(product = p)
+                }
             }
-        }
-    }
+        },
+        modifier = modifier
+    )
 }
 
 @Preview(showBackground = true)
